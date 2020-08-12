9f534213a2f85f9a6e6fed5a9a9cd86cde22575d
Rex Cardan <rexcardan@gmail.com>
2020-08-12 11:02:45 -0700

    Final product with no blue structures

---
+++ ViewModels/NoBlueStructureConstraint.cs
@@ -0,0 +1,40 @@
+﻿using ESAPIX.Constraints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using VMS.TPS.Common.Model.API;
+
+namespace Cardan.PlanChecker18.ViewModels
+{
+    public class NoBlueStructureConstraint : IConstraint
+    {
+        public string Name => "No Blue Constraints!";
+
+        public string FullName => "No Blue Constraints!";
+
+        public ConstraintResult CanConstrain(PlanningItem pi)
+        {
+            var pq = new PQAsserter(pi);
+            return pq.HasStructureSet()
+                .CumulativeResult;
+        }
+
+        public ConstraintResult Constrain(PlanningItem pi)
+        {
+            var ss = pi.StructureSet;
+            var hasBlue = ss.Structures.Any(s =>
+            {
+                return s.Color == Colors.Blue;
+            });
+
+            if (hasBlue) { return new ConstraintResult(this, ResultType.ACTION_LEVEL_3, "Has blue structure! Abort! Abort!!."); }
+            else
+            {
+                return new ConstraintResult(this, ResultType.PASSED, "Whew! No blue structures");
+            }
+        }
+    }
+}
